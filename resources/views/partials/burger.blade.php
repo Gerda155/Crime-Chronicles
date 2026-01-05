a8da60e58904ad73180a694c462c4757d4cbc844
Gerda Fedotova <gerda.fedotova.sk@lvt.lv>
2026-01-05 14:23:31 +0200

    Added roled

--- resources/views/partials/burger.blade.php
+++ resources/views/partials/burger.blade.php
@@ -7,29 +7,32 @@
     <div class="offcanvas-body">
         <a href="{{ route('profile.edit') }}" class="d-block text-light mb-2">Mans profils</a>
 
-        <a href=" {{ route('cases.index') }}" class="d-block text-light mb-2">
-            Visas lietas
-        </a>
+        <a href="{{ route('cases.index') }}" class="d-block text-light mb-2">Visas lietas</a>
 
-        <a href=" {{ route('cases.my-cases') }}" class="d-block text-light mb-2">
-            Manas lietas
-        </a>
+        <a href="{{ route('cases.my-cases') }}" class="d-block text-light mb-2">Manas lietas</a>
 
-        <a href=" {{ route('leaderboard') }}" class="d-block text-light mb-2">
-            Labākie detektīvi
-        </a>
+        <a href="{{ route('leaderboard') }}" class="d-block text-light mb-2">Labākie detektīvi</a>
 
-        <a href="{{ route('achievements.index') }}" class="d-block text-light mb-2">
-            Sasniegumi
-        </a>
+        <a href="{{ route('achievements.index') }}" class="d-block text-light mb-2">Sasniegumi</a>
 
-        <form method=" GET" action="{{ route('users.search') }}" class="mb-3">
+        @if(Auth::user()->role === 'moderator')
+            <a href="{{ route('leaderboard') }}" class="d-block text-light mb-2">
+                Dashboard (Moderator)
+            </a>
+        @endif
+
+        @if(Auth::user()->role === 'admin')
+            <a href="{{ route('leaderboard') }}" class="d-block text-light mb-2">
+                Dashboard (Admin)
+            </a>
+        @endif
+
+        <form method="GET" action="{{ route('users.search') }}" class="mb-3">
             <p>Meklēt detektīvus pēc sēgvārda:</p>
             <input
                 type="text"
                 name="q"
                 class="form-control bg-dark text-light border-secondary"
-                placeholder="Пошук за юзернеймом..."
                 value="{{ request('q') }}">
         </form>
     </div>
